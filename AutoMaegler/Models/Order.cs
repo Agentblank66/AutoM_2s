c8d34550a699bf8aa58755df4b46265004f85234
AgentBlank66 <watchandlearn626@gmail.com>
2025-04-29 11:56:14 +0200

    Created Order Model Class

---
+++ AutoMaegler/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿namespace AutoMaegler.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        Car Car { get; set; }
+        Employee Employee { get; set; }
+        Customer Customer { get; set; }
+
+        public Order(int id, Car car, Employee employee, Customer customer) 
+        {
+            Id = id;
+            Car = car;
+            Employee = employee;
+            Customer = customer;
+        }
+
+        public Order() 
+        { }
+
+        public override string ToString()
+        {
+            return $"Id: {Id} Car: {Car} Employee: {Employee} Customer: {Customer}";
+        }
+    }
+}
