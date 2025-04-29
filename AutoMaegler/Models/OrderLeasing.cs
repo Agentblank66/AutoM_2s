b8fa2df43f80242fd4cec432ceb85d98b8a89637
AgentBlank66 <watchandlearn626@gmail.com>
2025-04-29 12:41:15 +0200

    Changed LeasingPrice to Depositum

--- AutoMaegler/Models/OrderLeasing.cs
+++ AutoMaegler/Models/OrderLeasing.cs
@@ -2,13 +2,13 @@
 {
     public class OrderLeasing : Order
     {
-        public int LeasingPrice { get; set; }
+        public int Dipositum { get; set; }
         public DateTime LeasingDate { get; set; }
 
         public OrderLeasing(int id, Car car, Employee employee, Customer customer, int leasingPrice, DateTime leasingDate)
             : base(id, car, employee, customer)
         {
-            LeasingPrice = leasingPrice;
+            Dipositum = leasingPrice;
             LeasingDate = leasingDate;
         }
     }
