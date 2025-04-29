798e73d277d802a0d78799202c482b4417f087a6
Rasmus Dyring Raavig <rar002@edu.zealand.dk>
2025-04-29 11:55:49 +0200

    I have made a Service folder, where i've added a CarService class, ICarSerivce interface and a DBCarService class. where ive added methods to them.

---
+++ AutoMaegler/Service/ICarService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMaegler.Models;
+
+namespace AutoMaegler.Service
+{
+    public interface ICarService
+    {
+        public List<Car> GetCars();
+        public void AddCar(Car car);
+        public void UpdateCar(Car car);
+        public Car GetCar(int id);
+        public Car DeleteCar(int id);
+        public IEnumerable<Car> NameSearch(string str);
+        public IEnumerable<Car> PriceFilter(int minPrice, int maxPrice);
+
+    }
+}
