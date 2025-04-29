4473b7fdc135761975bf2afd6280635a9d6f04c9
Agentblank66 <watchandlearn626@gmail.com>
2025-04-29 16:07:24 +0200

    Created IOrderService Interface with methods without body'

---
+++ AutoMaegler/Service/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using AutoMaegler.Models;
+
+namespace AutoMaegler.Service
+{
+    public interface IOrderService
+    {
+        public List<Order> GetOrders();
+        public void AddOrder(Order order);
+        public void UpdateOrder(Order order);
+        public Order GetOrder(int id);
+        public Order DeleteOrder(int id);
+        public IEnumerable<Order> NameSearch(string str);
+        public IEnumerable<Order> PriceFilter(int minPrice, int maxPrice);
+    }
+}
