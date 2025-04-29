8e4f3aba7d87d4e2f8459ed05286e657f8946cc4
PowerBasss447 <Kasper.sandholt@outlook.com>
2025-04-29 11:32:31 +0200

    Tostring Car

--- AutoMaegler/Models/Customer.cs
+++ AutoMaegler/Models/Customer.cs
@@ -2,5 +2,26 @@
 {
     public class Customer
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public bool WishToSell { get; set; }
+
+        public Customer(int id, string name, int phonenumber, string email, string address, bool wishtosell)
+        {
+            Id = id;
+            Name = name;
+            PhoneNumber = phonenumber;
+            Email = email;
+            Address = address;
+            WishToSell = wishtosell;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id} Name: {Name} PhoneNumber: {PhoneNumber} Email: {Email} Address: {Address} WishToSell: {WishToSell}";
+        }
     }
 }
