26d7b7d642e95e361d1440c1560d634221a5d073
PowerBasss447 <Kasper.sandholt@outlook.com>
2025-04-29 10:34:59 +0200

    Oprettet klasse employee med instancefields, conctructor og Tostring

--- AutoMaegler/Employee.cs
+++ AutoMaegler/Employee.cs
@@ -2,5 +2,25 @@
 {
     public class Employee
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public Employee(int id, string name, string type, int phoneNumber, string email)
+        {
+            Id = id;
+            Name = name;
+            Type = type;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Id: {Id} Name: {Name} Type: {Type} PhoneNumber: {PhoneNumber} Email: {Email}";
+        }
     }
 }
