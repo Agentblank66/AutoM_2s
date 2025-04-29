5e212d94d93134f5ffe8731110401e4a6f0d7080
AgentBlank66 <watchandlearn626@gmail.com>
2025-04-29 12:00:42 +0200

    Changed Order class to an Abstract class and removed constructors.

--- AutoMaegler/Models/Order.cs
+++ AutoMaegler/Models/Order.cs
@@ -1,22 +1,11 @@
 ﻿namespace AutoMaegler.Models
 {
-    public class Order
+    public abstract class Order
     {
         public int Id { get; set; }
         Car Car { get; set; }
         Employee Employee { get; set; }
         Customer Customer { get; set; }
-
-        public Order(int id, Car car, Employee employee, Customer customer) 
-        {
-            Id = id;
-            Car = car;
-            Employee = employee;
-            Customer = customer;
-        }
-
-        public Order() 
-        { }
 
         public override string ToString()
         {
