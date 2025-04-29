5c3944c05e7b79b07f83bb2216377c3b35d9381b
OKronborg00 <oliverkronborg00@gmail.com>
2025-04-29 09:57:57 +0200

    Add Car class

---
+++ AutoMaegler/Car.cs
@@ -0,0 +1,17 @@
+﻿namespace AutoMaegler
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Car() { }
+            
+        public Car(int id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+    }
+}
