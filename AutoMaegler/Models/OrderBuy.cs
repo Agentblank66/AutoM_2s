75775d73dfa09919cc9a3fb93569bf32d5f342b7
AgentBlank66 <watchandlearn626@gmail.com>
2025-04-29 12:40:13 +0200

    Created OrderBuy class with constructor. Created OrderLeasing class with constructor.

---
+++ AutoMaegler/Models/OrderBuy.cs
@@ -0,0 +1,16 @@
+﻿namespace AutoMaegler.Models
+{
+    public class OrderBuy : Order
+    {
+        public int BuyPrice { get; set; }
+        public DateTime BuyDate { get; set; }
+
+        public OrderBuy(int id, Car car, Employee employee, Customer customer, int buyPrice, DateTime buyDate)
+        : base(id, car, employee, customer)
+        {
+            BuyPrice = buyPrice;
+            BuyDate = buyDate;
+        }
+    }
+
+}
---
+++ AutoMaegler/Models/OrderLeasing.cs
@@ -0,0 +1,15 @@
+﻿namespace AutoMaegler.Models
+{
+    public class OrderLeasing : Order
+    {
+        public int LeasingPrice { get; set; }
+        public DateTime LeasingDate { get; set; }
+
+        public OrderLeasing(int id, Car car, Employee employee, Customer customer, int leasingPrice, DateTime leasingDate)
+            : base(id, car, employee, customer)
+        {
+            LeasingPrice = leasingPrice;
+            LeasingDate = leasingDate;
+        }
+    }
+}
