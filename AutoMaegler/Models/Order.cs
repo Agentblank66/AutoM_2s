eb38052420b4f9c2624a2ad6aafd934f3ddd7dab
AgentBlank66 <watchandlearn626@gmail.com>
2025-04-30 11:51:18 +0200

    In Order class added property enum "leasingType" + change constructor

--- AutoMaegler/Models/Order.cs
+++ AutoMaegler/Models/Order.cs
@@ -2,17 +2,26 @@
 {
     public abstract class Order
     {
+        public enum leasingType
+        {
+            Leasing,
+            Buy,
+            Sale
+        }
+
+        public leasingType Type { get; set; }
         public int Id { get; set; }
         Car Car { get; set; }
         Employee Employee { get; set; }
         Customer Customer { get; set; }
 
-        public Order(int id, Car car, Employee employee, Customer customer)
+        public Order(int id, Car car, Employee employee, Customer customer, leasingType type)
         {
             Id = id;
             Car = car;
             Employee = employee;
             Customer = customer;
+            Type = type;
         }
     }
 }
