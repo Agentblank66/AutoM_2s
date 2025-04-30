ce919e6da137bcc290789fc43a991a9126442e82
Rasmus Dyring Raavig <rar002@edu.zealand.dk>
2025-04-30 18:07:48 +0200

    merching conflict

--- AutoMaegler/Service/OrderService.cs
+++ AutoMaegler/Service/OrderService.cs
@@ -38,28 +38,70 @@ namespace AutoMaegler.Service
             }
         }
 
-        public Order GetOrder(int id)
+        public Order GetOrder(int id, Order.OrderType type)
         {
-            foreach (Order order in _orders)
+            switch (type)
             {
-                if (order.Id == id)
-                {
-                    return order;
-                }
+                case Order.OrderType.Leasing:
+                    foreach (OrderLeasing order in _orderLeasings)
+                    {
+                        if (order.Id == id)
+                        {
+                            return order;
+                        }
+                    }
+                    break;
+                case Order.OrderType.Buy:
+                    foreach (OrderBuy order in _orderBuys)
+                    {
+                        if (order.Id == id)
+                        {
+                            return order;
+                        }
+                    }
+                    break;
+                case Order.OrderType.Sale:
+                    foreach (OrderSale order in _orderSales)
+                    {
+                        if (order.Id == id)
+                        {
+                            return order;
+                        }
+                    }
+                    break;
+                default:
+                    throw new ArgumentException("Invalid order type");
             }
             return null;
+
         }
 
         public void UpdateOrder(Order order)
         {
-            if (order != null)
+            Order orderToUpdate = GetOrder(order.Id, order.Type);
+            if (orderToUpdate != null)
             {
-                foreach (Order o in _orders)
-                {
-                    if (o.Id == order.Id)
-                    {
+                orderToUpdate.Car = order.Car;
+                orderToUpdate.Employee = order.Employee;
+                orderToUpdate.Customer = order.Customer;
 
-                    }
+                if (order is OrderLeasing)
+                {
+                    OrderLeasing leasingOrder = (OrderLeasing)order;
+                    ((OrderLeasing)orderToUpdate).Dipositum = leasingOrder.Dipositum;
+                    ((OrderLeasing)orderToUpdate).LeasingDate = leasingOrder.LeasingDate;
+                }
+                else if (order is OrderBuy)
+                {
+                    OrderBuy buyOrder = (OrderBuy)order;
+                    ((OrderBuy)orderToUpdate).BuyPrice = buyOrder.BuyPrice;
+                    ((OrderBuy)orderToUpdate).BuyDate = buyOrder.BuyDate;
+                }
+                else if (order is OrderSale)
+                {
+                    OrderSale saleOrder = (OrderSale)order;
+                    ((OrderSale)orderToUpdate).SalePrice = saleOrder.SalePrice;
+                    ((OrderSale)orderToUpdate).SaleDate = saleOrder.SaleDate;
                 }
             }
         }
