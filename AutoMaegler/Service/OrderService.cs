6f630f90b1b189fc5d3bcd3a8e1e3dcc0ada4eec
AgentBlank66 <watchandlearn626@gmail.com>
2025-04-30 12:09:26 +0200

    Added list and methods to Orderservice class

--- AutoMaegler/Service/OrderService.cs
+++ AutoMaegler/Service/OrderService.cs
@@ -1,6 +1,66 @@
-﻿namespace AutoMaegler.Service
+﻿using AutoMaegler.Models;
+
+namespace AutoMaegler.Service
 {
-    public class OrderService
+    public class OrderService : IOrderService
     {
+        private List<Order> _orders;
+        private List<OrderBuy> _orderBuys;
+        private List<OrderLeasing> _orderLeasings;
+        private List<Order> _orderSales;
+
+        public OrderService()
+        {
+        }
+
+        public List<Order> GetOrders()
+        {
+            return _orders;
+
+        }
+
+        public void AddOrder(Order order)
+        {
+            switch (order.Type)
+            {
+                case 0:
+                    _orderLeasings.Add((OrderLeasing)order);
+                    break;
+                case (Order.leasingType)1:
+                    _orderBuys.Add((OrderBuy)order);
+                    break;
+                case (Order.leasingType)2:
+                    _orderSales.Add((OrderSale)order);
+                    break;
+                default:
+                    throw new ArgumentException("Invalid order type");
+            }
+        }
+
+        public Order GetOrder(int id)
+        {
+            foreach (Order order in _orders)
+            {
+                if (order.Id == id)
+                {
+                    return order;
+                }
+            }
+            return null;
+        }
+
+        public void UpdateOrder(Order order) 
+        {
+            if (order != null)
+            {
+               foreach (Order o in _orders)
+                {
+                    if (o.Id == order.Id)
+                    {
+                        
+                    }
+                }
+            }
+        }
     }
 }
