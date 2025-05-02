66da4ae1e55d5a4868babe99a8a8b3fbd5f51e91
Agentblank66 <watchandlearn626@gmail.com>
2025-05-02 11:21:51 +0200

    Added commentary

--- AutoMaegler/Models/Order.cs
+++ AutoMaegler/Models/Order.cs
@@ -2,6 +2,9 @@
 {
     public abstract class Order
     {
+        /// <summary>
+        /// Represents the type of order.
+        /// </summary>
         public enum OrderType
         {
             Leasing,
@@ -9,12 +12,23 @@
             Sale
         }
 
+        /// <summary>
+        /// Properties of the Order class.
+        /// </summary>
         public OrderType Type { get; set; }
         public int Id { get; set; }
         public Car Car { get; set; }
         public Employee Employee { get; set; }
         public Customer Customer { get; set; }
 
+        /// <summary>
+        /// Constructor for the Order class.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="car"></param>
+        /// <param name="employee"></param>
+        /// <param name="customer"></param>
+        /// <param name="type"></param>
         public Order(int id, Car car, Employee employee, Customer customer, OrderType type)
         {
             Id = id;
