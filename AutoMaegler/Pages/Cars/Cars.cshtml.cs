4e671e6007fb1cd92762fa3a2675498b91806bd2
OKronborg00 <oliverkronborg00@gmail.com>
2025-05-09 09:00:35 +0200

    Car stuff Footer Abeselut

--- AutoMaegler/Pages/Cars/Cars.cshtml.cs
+++ AutoMaegler/Pages/Cars/Cars.cshtml.cs
@@ -1,3 +1,4 @@
+using AutoMaegler.Service;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,28 @@ namespace AutoMaegler.Pages.Cars
 {
     public class CarsModel : PageModel
     {
+        private ICarService _carService;
+
+        public void GetAllItemsModel(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        public List<Models.Car>? Cars { get; private set; }
+
+        [BindProperty]
+        public string SearchString { get; set; }
+            
+        [BindProperty]
+        public int MinPrice { get; set; }
+
+        [BindProperty]
+        public int MaxPrice { get; set; }
+
+
         public void OnGet()
         {
+            Cars = _carService.GetCars();
         }
     }
 }
