63188fdce3e7565b6bd3eb463c27bbecf19fbd18
Agentblank66 <watchandlearn626@gmail.com>
2025-05-14 00:18:23 +0200

    Created a folder named Comerators. Created 2 classes named: NameComperator and PriceComperator that inherits from IComparer, both classes have an Compare method.

---
+++ AutoMaegler/Comperators/PriceComperator.cs
@@ -0,0 +1,18 @@
+﻿using AutoMaegler.Models;
+
+namespace AutoMaegler.Comperators
+{
+    public class PriceComperator : IComparer<Order>
+    {
+        /// <summary>
+        /// Compares two orders by the car's price.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns> It return a int descriping wheter they are a like or not </returns>
+        public int Compare(Order x, Order y)
+        {
+            return x.Car.Price.CompareTo(y.Car.Price);
+        }
+    }
+}
