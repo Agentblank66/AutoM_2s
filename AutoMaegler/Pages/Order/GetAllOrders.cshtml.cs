64b171598b863bd0fc703e4d1c40342a6818c4dc
Agentblank66 <watchandlearn626@gmail.com>
2025-05-14 00:21:23 +0200

    Added a few additional properties and two methods: OnPostNameSearch and OnPostPriceFilter

--- AutoMaegler/Pages/Order/GetAllOrders.cshtml.cs
+++ AutoMaegler/Pages/Order/GetAllOrders.cshtml.cs
@@ -25,8 +25,62 @@ namespace AutoMaegler.Pages.Order
         /// </summary>
         public List<Models.Order>? Orders { get; private set; }
 
+        /// <summary>
+        /// The search string for the orders
+        /// </summary>
+        [BindProperty]
+        public string SearchString { get; set; }
+
+        /// <summary>
+        /// The minimum price for the orders
+        /// </summary>
+        [BindProperty]
+        public double MinPrice { get; set; }
+
+        /// <summary>
+        /// The maximum price for the orders
+        /// </summary>
+        [BindProperty]
+        public double MaxPrice { get; set; }
+
+        /// <summary>
+        /// The minimum leasing for the orders
+        /// </summary>
+        [BindProperty]
+        public double MinLeasing { get; set; }
+
+        /// <summary>
+        /// The maximum leasing for the orders
+        /// </summary>
+        [BindProperty]
+        public double MaxLeasing { get; set; }
+
+        /// <summary>
+        /// Searches for orders by customer name
+        /// </summary>
+        /// <returns> To the same newly updated page </returns>
+        public IActionResult OnPostNameSearch()
+        {
+            Orders = _orderService.NameSearch(SearchString).ToList();
+            return Page();
+        }
+
+        /// <summary>
+        /// Filters the price of the orders
+        /// </summary>
+        /// <returns> To the same newly updated page </returns>
+        public IActionResult OnPostPriceFilter()
+        {
+            Orders = _orderService.PriceFilter(MinPrice, MaxPrice, MinLeasing, MaxLeasing).ToList();
+            return Page();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public void OnGet()
         {
+            Orders = _orderService.GetOrders();
         }
     }
 }
