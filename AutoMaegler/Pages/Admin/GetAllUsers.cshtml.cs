9b4d4e394f1b7577e390967b35789ea656ca7d29
Rasmus Dyring Raavig <rar002@edu.zealand.dk>
2025-05-18 14:14:32 +0200

    no changes

--- AutoMaegler/Pages/Admin/GetAllUsers.cshtml.cs
+++ AutoMaegler/Pages/Admin/GetAllUsers.cshtml.cs
@@ -6,14 +6,22 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AutoMaegler.Pages.Admin
 {
+    /// <summary>
+    /// This class is used to get all users from the database, where only Employees can go to the page.
+    /// </summary>
     [Authorize(Roles = "Employee")]
     public class GetAllUsersModel : PageModel
     {
-       
+        /// <summary>
+        /// Property of the GetAllUsersModel class.
+        /// </summary>
         public UserService UserService { get; set; }
-        
 
 
+        /// <summary>
+        /// Constructor of the GetAllUsersModel class.
+        /// </summary>
+        /// <param name="userService"></param>
         public GetAllUsersModel(UserService userService)
         {
             UserService = userService;
