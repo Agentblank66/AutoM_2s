de3e92bf228be2bdb75f8bd1ad89c8cf63f46f42
Rasmus Dyring Raavig <rar002@edu.zealand.dk>
2025-05-19 13:42:54 +0200

    passwordhasher added

--- AutoMaegler/MockData/MockUsers.cs
+++ AutoMaegler/MockData/MockUsers.cs
@@ -1,10 +1,11 @@
 ﻿using AutoMaegler.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace AutoMaegler.MockData
 {
     public class MockUsers
-
     {
+        private static PasswordHasher<string> passwordHasher = new PasswordHasher<string>();
 
         /// <summary>
         /// A privat static list of employees.
