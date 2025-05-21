dc88049dc126efc7728e89b726ae99934732bfa3
PowerBasss447 <Kasper.sandholt@outlook.com>
2025-05-21 10:12:58 +0200

    Razor Om OS

---
+++ AutoMaegler/Pages/SellCars/SellCar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AutoMaegler.Pages.SellCars
+{
+    public class SellCarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
