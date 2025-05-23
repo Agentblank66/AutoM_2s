e62f5315cb457360b7a2da77233c764f21f24831
Rasmus Dyring Raavig <rar002@edu.zealand.dk>
2025-05-23 10:25:37 +0200

    adding new razor page

---
+++ AutoMaegler/Pages/Users/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AutoMaegler.Pages.Users.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
