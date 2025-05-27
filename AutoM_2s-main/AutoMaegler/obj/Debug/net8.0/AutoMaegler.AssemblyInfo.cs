ce5dc0acf3cb1dd3a5b8f905d454cedce7ef986f
PowerBasss447 <Kasper.sandholt@outlook.com>
2025-05-27 14:39:42 +0200

    fix

--- AutoM_2s-main/AutoMaegler/obj/Debug/net8.0/AutoMaegler.AssemblyInfo.cs
+++ AutoM_2s-main/AutoMaegler/obj/Debug/net8.0/AutoMaegler.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoMaegler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e1537905e0fc13dcb4cc23b215af2a5eac2e6d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+285e5dd51ad32638d1da2ee07fbeced4635657f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoMaegler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoMaegler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
