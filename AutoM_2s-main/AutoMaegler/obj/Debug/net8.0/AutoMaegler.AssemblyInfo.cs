06f337340d65893cf960d670e24ac635bec4a23c
Kasper Sandholt <kasp37a5@edu.zealand.dk>
2025-05-27 14:36:43 +0200

    Car

--- AutoM_2s-main/AutoMaegler/obj/Debug/net8.0/AutoMaegler.AssemblyInfo.cs
+++ AutoM_2s-main/AutoMaegler/obj/Debug/net8.0/AutoMaegler.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoMaegler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+105d1b8e7caf5fb21711dfb123a1103dba161ae7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c277b87b9ba154546e42e091b0535b3da93c67ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoMaegler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoMaegler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
